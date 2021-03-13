e32b16331186b49852737b31586bca5450c8a2d2
cardonajs <Jenscardona@gmail.com>
2021-03-13 14:10:57 -0500

    19.1

---
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,20 @@
+// Create a variable below
+
+
+var helloWorld = 'Hello its me'
+
+
+// Console.log() your variable here.
+
+console.log(helloWorld)
+
+var x = 3;
+var y = 2;
+var z = 'There are' + (x + y) + 'planes';
+
+
+function addplanes() {
+console.log(z);
+}
+
+addplanes();
---
+++ 19.1-activities/03_Using_jQuery/unsolved/js/index.js
@@ -0,0 +1,15 @@
+// Use jQuery to target the <h1> tag and make it red.
+$("h1").css ("color","red");
+
+
+// Use jQuery to target the blue class and make it blue.
+$(".blue").css("color","blue");
+
+
+// Use jQuery to target the addMe ID and change its HTML to Hello.
+$("#addme") .on('click',function(){
+
+$("h1").css ("color","red");
+$(".blue").css("color","blue");
+$("#addme") .html("Audios");
+});
